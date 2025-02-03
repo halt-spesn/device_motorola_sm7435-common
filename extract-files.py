0622d316746a3c60e1c4c3cf80a0111757dcfc32
dianlujitao <dianlujitao@lineageos.org>
2025-02-03 20:44:02 +0800

    sm7435-common: Fix loading USB audio AIDL interface with prebuilt HAL
    Change-Id: I0fe5631dad6446b5eda1fe8929f87cc974c29b3e

--- extract-files.py
+++ extract-files.py
@@ -107,6 +107,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libbluetooth_audio_session_aidl_qti.so',
     ): blob_fixup()
         .replace_needed('android.hardware.bluetooth.audio-V4-ndk.so', 'android.hardware.bluetooth.audio-V5-ndk.so'),
+    'vendor/lib64/libaudioserviceexampleimpl.so': blob_fixup()
+        .replace_needed('android.hardware.bluetooth.audio-V4-ndk.so', 'android.hardware.bluetooth.audio-V5-ndk.so')
+        .replace_needed('android.hardware.audio.core.sounddose-V2-ndk.so', 'android.hardware.audio.core.sounddose-V3-ndk.so')
+        .replace_needed('android.hardware.audio.core-V2-ndk.so', 'android.hardware.audio.core-V3-ndk.so'),
     (
         'vendor/bin/qguard',
         'vendor/lib64/libqcodec2_utils.so',
