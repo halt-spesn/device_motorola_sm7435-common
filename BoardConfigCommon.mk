8c706cef6b395104742197f5fcb982ac3a873bd5
Arian <arian.kulmer@web.de>
2026-01-22 19:18:48 +0100

    sm7435-common: Switch to libperfmgr
    Change-Id: I1f0a5fcf281f48e9610e521ec0ef07d7e1ae6dc0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,6 +159,7 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 
--- common.mk
+++ common.mk
@@ -308,7 +308,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # QSPA
 PRODUCT_PACKAGES += \
@@ -316,7 +317,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspa-service
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/parrot/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # RFS MSM MPSS symlinks
 PRODUCT_PACKAGES += \
@@ -351,7 +352,11 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/motorola
+    hardware/motorola \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
