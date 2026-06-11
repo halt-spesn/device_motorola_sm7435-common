5b423be197f9bcfe8cf1dfccf737e6687555b965
HALt <102590462+halt-spesn@users.noreply.github.com>
2026-06-11 15:43:44 +0300

    rename sensors blob

--- extract-files.py
+++ extract-files.py
@@ -62,7 +62,7 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio|dolby_audio).+\n', ''),
     'vendor/etc/sensors/hals.conf': blob_fixup()
-        .add_line_if_missing('sensors.moto_ext.so'),
+        .add_line_if_missing('sensors.motorola.v2.so'),
     'system_ext/priv-app/ims/ims.apk': blob_fixup()
         .apktool_patch('ims-patches'),
     (
