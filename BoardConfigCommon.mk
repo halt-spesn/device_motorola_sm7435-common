919b76ca8c577ca51f7a20d64ec104bc0640256c
HALt <102590462+halt-spesn@users.noreply.github.com>
2026-06-25 20:23:08 +0300

    i have no idea why we need qcom/touch-drivers

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,7 +127,6 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/dsp-kernel \
     qcom/opensource/graphics-kernel \
     qcom/opensource/spu-kernel \
-    qcom/opensource/touch-drivers \
     qcom/opensource/video-driver \
     qcom/opensource/wlan/platform \
     qcom/opensource/wlan/qcacld-3.0/.adrastea \
